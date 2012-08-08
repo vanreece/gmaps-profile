50b6aa120b64edd938806480f8ef71b2ed3dad23
Danny Krause <danny@carbonfive.com>
2012-08-08 11:05:34 -0700

    Using numpy

--- interp.py
+++ interp.py
@@ -1,4 +1,4 @@
-import sys
+import sys, numpy
 
 #1/3 arc second
 step = 1.0 / 60 / 60 / 3
@@ -10,16 +10,22 @@ entries = lines[1:]
 pairs = [map(float,x.split("\t")[1:3]) for x in entries]
 newpairs = []
 for i in range(len(pairs)-1):
-  begin_x,begin_y = pairs[i]
-  end_x,end_y = pairs[i+1]
-  delta_x = end_x - start_x
-  delta_y = end_y - start_y
-  distance = math.sqrt(delta_x*delta_x + delta_y*delta_y)
+  begin      = numpy.array(pairs[i])
+  end        = numpy.array(pairs[i+1])
+  difference = end - begin
+  distance   = numpy.linalg.norm(difference)
+  #print pairs[0]
   newpairs.append(pairs[0])
-  biggest_delta = max(math.abs(delta_x), math.abs(delta_y))
+  biggest_delta = max(numpy.abs(difference[0]), numpy.abs(difference[1]))
   t_step = step * distance / biggest_delta
   max_t = biggest_delta / step
   t = 0.0
   while t < max_t:
-    
-    newpairs.append(
+    pair = begin + difference * t
+    #print pair
+    newpairs.append(pair)
+    t += t_step
+#print pairs[-1]
+newpairs.append(pairs[-1])
+print len(newpairs)
+print newpairs
