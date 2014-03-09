c3f31fa07e3ba11e0dc0da01beb2e82d1a4b744f
Danny Krause <vanreece@gmail.com>
2014-03-09 10:50:27 -0700

    WIP

--- interp.py
+++ interp.py
@@ -9,23 +9,25 @@ lines = contents.split("\n")
 entries = lines[1:]
 pairs = [map(float,x.split("\t")[1:3]) for x in entries]
 newpairs = []
+total_distance = 0.0
 for i in range(len(pairs)-1):
   begin      = numpy.array(pairs[i])
   end        = numpy.array(pairs[i+1])
+  #skip identical pairs
+  if (begin == end).all():
+    continue
   difference = end - begin
   distance   = numpy.linalg.norm(difference)
-  #print pairs[0]
+  total_distance += distance
   newpairs.append(pairs[0])
   biggest_delta = max(numpy.abs(difference[0]), numpy.abs(difference[1]))
   t_step = step * distance / biggest_delta
   max_t = biggest_delta / step
-  t = 0.0
-  while t < max_t:
+  for t in numpy.linspace(0.0, max_t, biggest_delta / step):
     pair = begin + difference * t
-    #print pair
     newpairs.append(pair)
     t += t_step
-#print pairs[-1]
-newpairs.append(pairs[-1])
-print len(newpairs)
-print newpairs
+
+print "type\tlatitude\tlongitude\taltitude (m)\tname"
+for pair in newpairs:
+  print "T\t" + str(pair[0]) + "\t" + str(pair[1]) + "\t\t"
